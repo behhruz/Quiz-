b75ef08b2143b7c8a98ddf54583daa450cd02161
behhruz <bsotimboyev10@gmail.com>
2025-09-16 14:45:25 +0500

    done

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -13,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Swagger yuklash
+// Swagger
 const swaggerDocument = YAML.load(__dirname + "/docs/swagger.yaml");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
@@ -24,4 +25,16 @@ app.use("/question", questionRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+// MongoDB ulash
+mongoose
+
+    .connect( "mongodb+srv://bsotimboyev10_db_user:Ub0ervsVvTOotGAk@cluster0.oczjloo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("✅ MongoDB connected");
+        app.listen(PORT, () =>
+            console.log(`🚀 Server running on http://localhost:${PORT}`)
+        );
+    })
+    .catch((err) => console.error("❌ MongoDB connection error:", err.message));
+
+
